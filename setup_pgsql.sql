2cbb8df089b41fe0e8794ab3609613a8ea42b1b4
Mihail Gershkovich <Mihail.Gershkovich@gmail.com>
2022-01-27 13:30:19 +0100

    Update setup_pgsql.sql

--- setup_pgsql.sql
+++ setup_pgsql.sql
@@ -1,13 +1,13 @@
 -- the code is pretty self explanatory. Start the reverse engineering with the view and functions at the end of the file
-CREATE ROLE salesforce_sync; -- grant this role to the connecting user
-CREATE SCHEMA AUTHORIZATION salesforce_sync;
-SET ROLE salesforce_sync;
+CREATE ROLE salesforce2pg; -- grant this role to the connecting user
+CREATE SCHEMA AUTHORIZATION salesforce2pg;
+SET ROLE salesforce2pg;
 -- grant all required rights on all required schemas!!!
---DROP TABLE IF EXISTS salesforce_sync.sf_instance;
---DROP TABLE IF EXISTS salesforce_sync.api_column;
---DROP TABLE IF EXISTS salesforce_sync.api_table;
+--DROP TABLE IF EXISTS salesforce2pg.sf_instance;
+--DROP TABLE IF EXISTS salesforce2pg.api_column;
+--DROP TABLE IF EXISTS salesforce2pg.api_table;
 
-CREATE TABLE salesforce_sync.sf_instance (
+CREATE TABLE salesforce2pg.sf_instance (
     sf_instance varchar PRIMARY KEY
     , organization_id varchar NOT NULL
     , username varchar NOT NULL
@@ -17,22 +17,25 @@ CREATE TABLE salesforce_sync.sf_instance (
     , notes_instance varchar NULL
     )
 
-CREATE TABLE salesforce_sync.api_table(
-    sf_instance varchar REFERENCES salesforce_sync.sf_instance(sf_instance)
+CREATE TABLE salesforce2pg.api_table(
+    sf_instance varchar REFERENCES salesforce2pg.sf_instance(sf_instance)
     , source_table varchar PRIMARY KEY
     , target_table varchar
     , last_modified_column varchar
     , pk_column varchar
     , readers _varchar 
     , source_filter varchar
-    , enabled boolean DEFAULT TRUE
-    , rebuild boolean DEFAULT FALSE
+    , enabled boolean NOT NULL DEFAULT TRUE
+    , rebuild boolean NOT NULL DEFAULT FALSE
     , update_frequency INTERVAL DEFAULT '5 minutes'
-    , notes_table varchar NULL
+    , filter_notes varchar NULL
+	, api_status_message varchar NULL
+	, author varchar NULL DEFAULT SESSION_USER
+	, author_notes varchar NULL
 );
 
-CREATE TABLE salesforce_sync.api_column (
-    sf_instance varchar REFERENCES salesforce_sync.sf_instance(sf_instance)
+CREATE TABLE salesforce2pg.api_column (
+    sf_instance varchar REFERENCES salesforce2pg.sf_instance(sf_instance)
     , source_table varchar REFERENCES ciq.api_table(source_table)
     , source_column varchar
     , target_column varchar
@@ -43,7 +46,7 @@ CREATE TABLE salesforce_sync.api_column (
     , PRIMARY KEY (source_table, source_column)
 );
 
-CREATE OR REPLACE FUNCTION salesforce_sync.last_update(src_table character varying, OUT last_update timestamp without time zone)
+CREATE OR REPLACE FUNCTION salesforce2pg.last_update(src_table character varying, OUT last_update timestamp without time zone)
  RETURNS timestamp without time zone
  LANGUAGE plpgsql
  SECURITY DEFINER
@@ -52,8 +55,8 @@ declare
  runner_sql TEXT;
 BEGIN
 runner_sql := (SELECT 'SELECT max('||target_column||') FROM '||target_table||';'
-        FROM salesforce_sync.api_table apit
-        NATURAL JOIN salesforce_sync.api_column
+        FROM salesforce2pg.api_table apit
+        NATURAL JOIN salesforce2pg.api_column
         WHERE source_column=apit.last_modified_column
         AND source_table=src_table
     );
@@ -62,7 +65,7 @@ END;
 $function$
 ;
 
-CREATE OR REPLACE PROCEDURE salesforce_sync.rebuild_target()
+CREATE OR REPLACE PROCEDURE salesforce2pg.rebuild_target()
 -- rebuilds all tables at once;
  LANGUAGE plpgsql
  SECURITY DEFINER
@@ -84,21 +87,21 @@ SELECT 'DROP TABLE IF EXISTS '||target_table||';'||chr(10)
             FILTER (WHERE source_column=apit.last_modified_column)
         ||'GRANT SELECT ON '||target_table||' TO '||array_to_string(readers,', ')||';'
         ||chr(10) sql_runner
-        FROM salesforce_sync.api_table apit
-        NATURAL JOIN salesforce_sync.api_column
+        FROM salesforce2pg.api_table apit
+        NATURAL JOIN salesforce2pg.api_column
         WHERE rebuild
         GROUP BY apit.source_table
 LOOP
     EXECUTE code_snippet.sql_runner;
 END LOOP;
-UPDATE salesforce_sync.api_table SET rebuild=FALSE WHERE rebuild;
+UPDATE salesforce2pg.api_table SET rebuild=FALSE WHERE rebuild;
 END;
 $procedure$
 ;
 
--- CALL salesforce_sync.rebuild_target();
+-- CALL salesforce2pg.rebuild_target();
 
-CREATE OR REPLACE PROCEDURE salesforce_sync.rebuild_target(src_table character varying)
+CREATE OR REPLACE PROCEDURE salesforce2pg.rebuild_target(src_table character varying)
 -- rebuilds specific table
  LANGUAGE plpgsql
  SECURITY DEFINER
@@ -118,19 +121,19 @@ runner_sql := (SELECT 'DROP TABLE IF EXISTS '||target_table||';'||chr(10)
         || string_agg('CREATE INDEX on '||target_table||'('||target_column||');',chr(10))
             FILTER (WHERE source_column=apit.last_modified_column)
         ||'GRANT SELECT ON '||target_table||' TO '||array_to_string(readers,', ')||';'
-        FROM salesforce_sync.api_table apit
-        NATURAL JOIN salesforce_sync.api_column
+        FROM salesforce2pg.api_table apit
+        NATURAL JOIN salesforce2pg.api_column
         WHERE rebuild
         AND source_table=src_table
         GROUP BY apit.source_table
     );
 EXECUTE runner_sql;
-UPDATE salesforce_sync.api_table SET rebuild=FALSE WHERE source_table=src_table;
+UPDATE salesforce2pg.api_table SET rebuild=FALSE WHERE source_table=src_table;
 END;
 $procedure$
 ;
 
-CREATE OR REPLACE PROCEDURE salesforce_sync.upsert(src_table character varying, jsontext text)
+CREATE OR REPLACE PROCEDURE salesforce2pg.upsert(src_table character varying, jsontext text)
  LANGUAGE plpgsql
  SECURITY DEFINER
 AS $procedure$
@@ -151,8 +154,8 @@ runner_sql := (SELECT
     ON CONFLICT ('||pk_column||') DO UPDATE SET'||chr(10)
     ||string_agg(' '||target_column||' = EXCLUDED.'||target_column, ','||chr(10) ORDER BY column_order) FILTER (WHERE target_column<>pk_column)
     , jsontext::jsonb)
-    FROM salesforce_sync.api_table apit
-    NATURAL JOIN salesforce_sync.api_column
+    FROM salesforce2pg.api_table apit
+    NATURAL JOIN salesforce2pg.api_column
     WHERE enabled
     AND source_table=src_table
     GROUP BY apit.source_table);
@@ -162,8 +165,8 @@ $procedure$
 ;
 
 
-DROP VIEW salesforce_sync.api_query;
-CREATE OR REPLACE VIEW salesforce_sync.api_query AS
+DROP VIEW salesforce2pg.api_query;
+CREATE OR REPLACE VIEW salesforce2pg.api_query AS
 SELECT sf_instance, source_table
 --, update_frequency
 , concat('SELECT '||string_agg(source_column, ', ')||chr(10)
@@ -172,7 +175,7 @@ SELECT sf_instance, source_table
         -- we let the sync script overlap, in case not all changes propagated during the time when the sync synchronised last time.
         -- however this makes the process sync same data multiple times, until new changes arrive.
         -- the impact is minimal
-            COALESCE(salesforce_sync.last_update(api_table.source_table), '1900-01-01')
+            COALESCE(salesforce2pg.last_update(api_table.source_table), '1900-01-01')
                 -update_frequency::interval
                 -INTERVAL '5 minutes' -- the overlap, hardcoded on purpose
                 , 'YYYY-MM-DD"T"HH24:MI:SS"Z"'
@@ -181,9 +184,9 @@ SELECT sf_instance, source_table
     , 'AND '||source_filter|| chr(10)
     , 'ORDER BY '||last_modified_column||' DESC'
     ) source_query
-FROM salesforce_sync.api_table
-NATURAL JOIN salesforce_sync.api_column
-NATURAL JOIN salesforce_sync.sf_instance
+FROM salesforce2pg.api_table
+NATURAL JOIN salesforce2pg.api_column
+NATURAL JOIN salesforce2pg.sf_instance
 WHERE enabled
 GROUP BY source_table;
 
