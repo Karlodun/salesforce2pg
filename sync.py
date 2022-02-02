d172357ed0f0f885527ff751de43db8bb5fc937b
Mihail Gershkovich <Mihail.Gershkovich@gmail.com>
2022-02-03 00:35:32 +0100

    added error handling for SOQL API
    further error handling and debbuging features needed.

--- sync.py
+++ sync.py
@@ -4,22 +4,28 @@ import json, datetime, pg, sf
 
 def tables_to_pg(t):
     print(' table:', t['source_table'])
-    sf_res = sfi.query(t['source_query'], include_deleted=True)
-    while sf_res:
-        list_pos = 0
-        list_step = 1000
-        sf_records = len(sf_res["records"])
-        while list_pos < sf_records:
-            list_end = list_pos + list_step
-            dict_records = sf_res["records"][list_pos:list_end]
-            json_records = json.dumps(dict_records).replace("'", "''")
-            list_pos += list_step
-            pg.upsert(t['sf_instance'], t['source_table'], json_records)
-        if "nextRecordsUrl" in sf_res:
-            sf_res = sfi.query_more(sf_res["nextRecordsUrl"], True)
-        else:
-            sf_res = None
-
+    pg.update_table_api_status(t['sf_instance'], t['source_table'], 'processing')
+    try:
+        sf_res = sfi.query(t['source_query'], include_deleted=True)
+        
+        while sf_res:
+            list_pos = 0
+            list_step = 1000  # 1000 seems to be a good number to be processed by PostgreSQL
+            sf_records = len(sf_res["records"])
+            while list_pos < sf_records:
+                list_end = list_pos + list_step
+                dict_records = sf_res["records"][list_pos:list_end]
+                json_records = json.dumps(dict_records).replace("'", "''")
+                list_pos += list_step
+                pg.upsert(t['sf_instance'], t['source_table'], json_records)
+            if "nextRecordsUrl" in sf_res:
+                sf_res = sfi.query_more(sf_res["nextRecordsUrl"], True)
+            else:
+                sf_res = None
+    except Exception as e:
+        emsg = e.content[0]['message'].replace("'", "''")
+        pg.update_table_api_status(t['sf_instance'], t['source_table'], emsg)
+        
 
 def main():
     pg.rebuild()
@@ -32,5 +38,9 @@ def main():
 
     print('sync completed at:', datetime.now())
     
-if __name__="__main__":
+if __name__=="__main__":
     main()
+
+    
+if __name__ == "sync":
+    print('here should be some driving functionality')
