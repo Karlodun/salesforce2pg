25895a76aa4f3f847d0c1a614b4423f6f7b5ac81
Mihail Gershkovich <Mihail.Gershkovich@gmail.com>
2022-02-17 11:57:13 +0100

    Update sample_config.sql

--- sample_config.sql
+++ sample_config.sql
@@ -1,43 +1,19 @@
 -- Example configuration, please check for exact fields you need!:
-INSERT INTO ciq.salesforce_instances VALUES -- all fields
+INSERT INTO salesforce2pg.salesforce_instances VALUES -- all fields
 ('your_instance.my.salesforce.com', 'your_organization_id', 'your_username', 'your_pwd', 'session_id', 'security_token')
 
-INSERT INTO ciq.salesforce_instances VALUES -- most required
+INSERT INTO salesforce2pg.salesforce_instances VALUES -- most required
 ('your_instance.my.salesforce.com', 'your_organization_id', 'your_username', 'your_pwd')
 
-INSERT INTO ciq.api_table VALUES 
-('your_instance.my.salesforce.com', 'Account', 'account', 'LastModifiedDate', 'account_id', ARRAY['postgres'], 'BillingCountry = ''France'''),
-('your_instance.my.salesforce.com', 'Contact', 'contact', 'LastModifiedDate', 'contact_id', ARRAY['postgres'], 'Department = ''finance'''),
+INSERT INTO salesforce2pg.api_table VALUES 
+('your_instance.my.salesforce.com', 'BusObject', 'bus_object', 'LastModifiedDate', 'bus_object_id', ARRAY['postgres'], 'SomeAttribute = ''some_value'''),
 ;
 
-INSERT INTO ciq.api_column VALUES 
--- accounts
-('your_instance.my.salesforce.com','Account', 'Id', 'account_id', 1),
-('your_instance.my.salesforce.com','Account', 'IsDeleted', 'is_deleted',6),
-('your_instance.my.salesforce.com','Account', 'Name', 'office_name',15),
-('your_instance.my.salesforce.com','Account', 'BillingStreet', 'street',20),
-('your_instance.my.salesforce.com','Account', 'BillingPostalCode', 'postcode',21),
-('your_instance.my.salesforce.com','Account', 'BillingCity', 'city',22),
-('your_instance.my.salesforce.com','Account', 'BillingState', 'state',23),
-('your_instance.my.salesforce.com','Account', 'BillingCountry', 'country',24),
-('your_instance.my.salesforce.com','Account', 'BillingLatitude', 'lat',31),
-('your_instance.my.salesforce.com','Account', 'BillingLongitude', 'lon',32),
-('your_instance.my.salesforce.com','Account', 'BillingGeocodeAccuracy', 'gc_accuracy',33),
-('your_instance.my.salesforce.com','Account', 'Phone', 'phone',41),
-('your_instance.my.salesforce.com','Account', 'Website', 'url',42),
-('your_instance.my.salesforce.com','Account', 'LastModifiedDate', 'last_modified',51),
--- contacts
-('your_instance.my.salesforce.com','Contact', 'Id', 'contact_id', 1),
-('your_instance.my.salesforce.com','Contact', 'IsDeleted', 'is_deleted',6),
-('your_instance.my.salesforce.com','Contact', 'AccountID', 'account_id', 17),
-('your_instance.my.salesforce.com','Contact', 'Salutation', 'salutation', 20),
-('your_instance.my.salesforce.com','Contact', 'Title', 'title', 21),
-('your_instance.my.salesforce.com','Contact', 'FirstName', 'first_name', 22),
-('your_instance.my.salesforce.com','Contact', 'LastName', 'last_name', 23),
-('your_instance.my.salesforce.com','Contact', 'Name', 'full_name', 24),
-('your_instance.my.salesforce.com','Contact', 'Email', 'email', 25),
-('your_instance.my.salesforce.com','Contact', 'Phone', 'phone', 26),
-('your_instance.my.salesforce.com','Contact', 'MobilePhone', 'mobile', 27),
-('your_instance.my.salesforce.com','Contact', 'Department', 'department', 31),
-('your_instance.my.salesforce.com','Contact', 'LastModifiedDate', 'last_modified',51)
-('your_instance.my.salesforce.com','Contact', 'CreatedDate', 'created_date',71)
+INSERT INTO salesforce2pg.api_column VALUES 
+('your_instance.my.salesforce.com','BusObject', 'Id', 'account_id', 1),
+('your_instance.my.salesforce.com','BusObject', 'IsDeleted', 'is_deleted',6),
+('your_instance.my.salesforce.com','BusObject', 'Attr1', 'attribute_1',15),
+('your_instance.my.salesforce.com','BusObject', 'Attr7', 'attribute_512',20),
+('your_instance.my.salesforce.com','BusObject', 'Attr8', 'another_attribute',21),
+('your_instance.my.salesforce.com','BusObject', 'Attr52', 'and_another_one',42),
+('your_instance.my.salesforce.com','BusObject', 'LastModifiedDate', 'last_modified',51);
